476d7a127d48b5b8ab0eda6b8b35939c11af99e7
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-03-29 15:43:16 -0400

    debug

--- users/function_views.py
+++ users/function_views.py
@@ -2,14 +2,22 @@
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required, user_passes_test
+
+from rest_framework import status
 from rest_framework.response import Response
 
+from users.models import Customer
 from users.views import CustomerSerializer
+
 
 @api_view(['GET'])
 @csrf_exempt
-@login_required
-@user_passes_test(lambda user: user.is_active)
 def get_customer(request):
+    if not request.user.is_authenticated or not request.user.is_active:
+        return Response("Invalid Credentials", status=403)
+    
     user_serializer = CustomerSerializer(request.user)
+    if not user_serializer.is_valid():
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     return Response(user_serializer.data)
