9a63f811df57d46874aa97467da616c12f28fd2f
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-03-31 16:24:17 -0400

    test

--- users/webhooks.py
+++ users/webhooks.py
@@ -1,6 +1,8 @@
 from users.function_views import generate_qr
 from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def generate_qr_webhook(request):
     if request.method == 'POST':
         # Call the generate_qr view function
