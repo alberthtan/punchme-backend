30ffdd9b8b35681bdd59bca4be16103f6a6ac754
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-03-31 11:50:01 -0400

    access token debugging

--- punchme/settings.py
+++ punchme/settings.py
@@ -128,7 +128,7 @@ LOGGING = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(weeks=99),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(weeks=99),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True
