8d9d569f1c2bccf7d134be31378e2ee00db5b978
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-03-31 16:38:58 -0400

    test

--- users/webhooks.py
+++ users/webhooks.py
@@ -9,8 +9,9 @@ from users.models import Customer
 @csrf_exempt
 def generate_qr_webhook(request):
 
-    # Call the generate_qr function
-    response = generate_qr(request)
+    # Call the generate_qr function with the HttpRequest object
+    response = generate_qr(request._request)
 
     # Return a response to confirm that the webhook was received and processed correctly
     return Response("Webhook received and processed.", status=200)
+
