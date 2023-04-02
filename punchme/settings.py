16f560b7877278a98b5d5dd4eb40f540e863cb3d
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-04-02 19:33:46 -0400

    access and refresh token expiration to prod

--- punchme/settings.py
+++ punchme/settings.py
@@ -128,8 +128,8 @@ LOGGING = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=60),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True
 }
