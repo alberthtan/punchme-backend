940750a5e58e8b832c79a60c05b027e38abb5b87
Albert Tan <alberttan@Alberts-MacBook-Pro.local>
2023-04-16 19:46:05 -0400

    android 10

--- users/login_views.py
+++ users/login_views.py
@@ -234,8 +234,7 @@ class SendEmailCode(CreateAPIView):
             email_auth = EmailAuthentication.objects.create(
                 email=email,
             )
-            print(manager)
-            while email_auth.code == manager.employee_code:
+            while email_auth.code == manager[0].employee_code:
                 print("hello")
                 EmailAuthentication.objects.filter(email=email).delete()
                 email_auth = EmailAuthentication.objects.create(
